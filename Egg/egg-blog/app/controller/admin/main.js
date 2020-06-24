4c6bc3030487a23885a1c4331cddd4fc32e96c66
zhouguangyao <wyprivatemail@163.com>
2020-06-24 10:07:36 +0800

    Egg: egg-blog update  admin controller

--- Egg/egg-blog/app/controller/admin/main.js
+++ Egg/egg-blog/app/controller/admin/main.js
@@ -28,6 +28,50 @@ class MainController extends Controller {
     const data = await app.mysql.select('type');
     ctx.body = data;
   }
+  async addArticle() {
+    const { ctx, app } = this;
+    const data = ctx.request.body;
+    const { insertId: id, affectedRows } = await app.mysql.insert('article', data);
+    if (affectedRows === 1) {
+      ctx.body = { id, message: '发布文章成功' };
+    } else {
+      ctx.body = {
+        error: true,
+        message: '发布文章失败',
+      };
+    }
+  }
+  async getArticles() {
+    const { ctx, app } = this;
+    const sql = 'SELECT article.id as id,' +
+                'type.typeName as typeName,' +
+                'article.title as title,' +
+                'FROM_UNIXTIME(article.addTime, \'%Y-%m-%d\') as addTime,' +
+                'article.view_count as view_count ' +
+                'FROM article LEFT JOIN type ON article.type_id = type.id ' +
+                'ORDER BY article.id DESC';
+    const data = await app.mysql.query(sql);
+    ctx.body = data;
+  }
+  async delArticle() {
+    const { ctx, app } = this;
+    const { id } = ctx.request.body;
+    const data = await app.mysql.delete('article', { id });
+    ctx.body = data;
+  }
+  async getArticle() {
+    const { ctx, app } = this;
+    const { id } = ctx.params;
+    const sql = 'SELECT article.id as id,' +
+                'article.type_id as type_id,' +
+                'article.title as title,' +
+                'article.article_content as article_content,' +
+                'article.introduce as introduce,' +
+                'FROM_UNIXTIME(article.addTime, \'%Y-%m-%d\') as addTime ' +
+                'FROM article WHERE article.id = ' + id;
+    const [ data ] = await app.mysql.query(sql);
+    ctx.body = data;
+  }
 }
 
 module.exports = MainController;
--- Egg/egg-blog/app/router/admin.js
+++ Egg/egg-blog/app/router/admin.js
@@ -9,4 +9,8 @@ module.exports = app => {
   router.get('/admin/index', auth, controller.admin.main.index);
   router.post('/admin/login', controller.admin.main.login);
   router.get('/admin/type', auth, controller.admin.main.type);
+  router.post('/admin/article', auth, controller.admin.main.addArticle);
+  router.get('/admin/articles', auth, controller.admin.main.getArticles);
+  router.delete('/admin/article', auth, controller.admin.main.delArticle);
+  router.get('/admin/article/:id', auth, controller.admin.main.getArticle);
 };
